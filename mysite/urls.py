2e0ef11b3391477c62654543a94cbf6ae2cd1417
Taylor Allred <2000ponyz@gmail.com>
2014-11-12 09:40:53 -0700

    first commit

---
+++ mysite/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+
+urlpatterns = patterns('',
+    url(r'^polls/', include('polls.urls', namespace="polls")),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace="rest_framework"))
+)
